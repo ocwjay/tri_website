141ad2f5d72c0fb8141231878ea88b9ae0e8c1c9
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-06-07 20:04:02 -0500

    fixed animation for closing nav

--- actions.js
+++ actions.js
@@ -1,16 +1,31 @@
 document.body.addEventListener("keydown", keyIn);
+var open = 0;
 function keyIn(){
   var keyWas = event.which;
   if(keyWas == 13){
     document.getElementById("overlay").style.display = "flex";
-    var time = setInterval(frame, 50);
+    var openTime = setInterval(openFrame, 50);
     var opa = 0.0;
-    function frame(){
+    function openFrame(){
       if(opa >= 1.0){
-        clearInterval(time);
+        clearInterval(openTime);
       }
       else{
         opa += 0.1;
+        document.getElementById("overlay").style.opacity = opa;
+      }
+    }
+  }
+  else if (keyWas == 8) {
+    var openTime = setInterval(closeFrame, 50);
+    var opa = 1.0;
+    function closeFrame(){
+      if(opa <= 0.0){
+        clearInterval(closeTime);
+        document.getElementById("overlay").style.display = "none";
+      }
+      else{
+        opa -= 0.1;
         document.getElementById("overlay").style.opacity = opa;
       }
     }
