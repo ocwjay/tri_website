29f647874d045535b49e5a1adc4b02c34b7b9873
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-06-07 23:36:38 -0500

    updated fun stuff with the team page

--- actions.js
+++ actions.js
@@ -17,7 +17,7 @@ function keyIn(){
     }
   }
   else if (keyWas == 8) {
-    var openTime = setInterval(closeFrame, 50);
+    var closeTime = setInterval(closeFrame, 50);
     var opa = 1.0;
     function closeFrame(){
       if(opa <= 0.0){
@@ -30,4 +30,41 @@ function keyIn(){
       }
     }
   }
+}
+
+document.getElementById("topTriangle").addEventListener("mouseover", topTriangleBoxChange);
+document.getElementById("topTriangle").addEventListener("mouseout", mouseOutBoxChange);
+document.getElementById("middleTriangle").addEventListener("mouseover", middleTriangleBoxChange);
+document.getElementById("middleTriangle").addEventListener("mouseout", mouseOutBoxChange);
+document.getElementById("bottomTriangle").addEventListener("mouseover", bottomTriangleBoxChange);
+document.getElementById("bottomTriangle").addEventListener("mouseout", mouseOutBoxChange);
+function topTriangleBoxChange(){
+  var teamHeader = document.getElementById("memberName");
+  var teamParagraph = document.getElementById("memberDescription");
+  teamHeader.innerHTML = "Gustavo";
+  teamParagraph.innerHTML = "blah blah blah Felipe will write this I hope xD"
+  teamHeader.style.opacity = 1.0;
+  teamParagraph.style.opacity = 1.0;
+}
+function middleTriangleBoxChange(){
+  var teamHeader = document.getElementById("memberName");
+  var teamParagraph = document.getElementById("memberDescription");
+  teamHeader.innerHTML = "Jena";
+  teamParagraph.innerHTML = "blah blah blah Felipe will write this I hope xD"
+  teamHeader.style.opacity = 1.0;
+  teamParagraph.style.opacity = 1.0;
+}
+function bottomTriangleBoxChange(){
+  var teamHeader = document.getElementById("memberName");
+  var teamParagraph = document.getElementById("memberDescription");
+  teamHeader.innerHTML = "Felipe";
+  teamParagraph.innerHTML = "blah blah blah Felipe will write this I hope xD"
+  teamHeader.style.opacity = 1.0;
+  teamParagraph.style.opacity = 1.0;
+}
+function mouseOutBoxChange(){
+  var teamHeader = document.getElementById("memberName");
+  var teamParagraph = document.getElementById("memberDescription");
+  teamHeader.innerHTML = "";
+  teamParagraph.innerHTML = "";
 }
