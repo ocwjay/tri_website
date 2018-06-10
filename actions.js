7e057cc060e7249e174337f5ae8a746f5a97de7e
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-06-10 18:09:32 -0500

    safety push

--- actions.js
+++ actions.js
@@ -3,32 +3,10 @@ var open = 0;
 function keyIn(event){
   var keyWas = event.which;
   if(keyWas == 13){
-    document.getElementById("overlay").style.display = "flex";
-    var openTime = setInterval(openFrame, 50);
-    var opa = 0.0;
-    function openFrame(){
-      if(opa >= 1.0){
-        clearInterval(openTime);
-      }
-      else{
-        opa += 0.1;
-        document.getElementById("overlay").style.opacity = opa;
-      }
-    }
+    alert("enter key was pressed");
   }
   else if (keyWas == 8) {
-    var closeTime = setInterval(closeFrame, 50);
-    var opa = 1.0;
-    function closeFrame(){
-      if(opa <= 0.0){
-        clearInterval(closeTime);
-        document.getElementById("overlay").style.display = "none";
-      }
-      else{
-        opa -= 0.1;
-        document.getElementById("overlay").style.opacity = opa;
-      }
-    }
+    alert("backspace key was pressed");
   }
 }
 
