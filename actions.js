d63b382793249d5683e9208f058a6b09d6f7b4d2
Jenalee Palmer <jenaleepalmer@gmail.com>
2018-06-11 22:07:11 -0500

    SAFETY PUSH some of it works now

--- actions.js
+++ actions.js
@@ -2,10 +2,13 @@ document.body.addEventListener("keydown", keyIn);
 function keyIn(event){
   var keyWas = event.which;
   var overlayElement = document.getElementById("overlay");
+  var openFlag = 0;
   if(keyWas == 13){
-    enter
+    overlayElement.classList.add("overlayOpen");
   }
   else if(keyWas == 8) {
-    //backspace
+    //add a class that uses a new animation
+    //then set a timeout to remove that class and the class below vvv
+    overlayElement.classList.remove("overlayOpen");
   }
 }
